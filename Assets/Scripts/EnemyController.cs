3ac769d2322e0490b44b00e6f60bfd931f7d294e
Felipe <wowaccount123555@hotmail.com>
2016-11-14 11:32:57 -0200

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private bool isDead = false;
+
+	private Transform player;
+	public float radiusOfSight;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		player = FindObjectOfType<PlayerController> ().gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) {
+			Raycasting ();
+		}
+	}
+
+	void Raycasting(){
+		Vector3 forward = new Vector3(player.position.x - gameObject.transform.position.x, 0, 0);
+		forward.Normalize ();
+		forward *= radiusOfSight;
+		Debug.DrawRay (gameObject.transform.position, forward, Color.green);
+
+		RaycastHit2D hit = Physics2D.Raycast (gameObject.transform.position, forward, radiusOfSight, 1 << LayerMask.NameToLayer("Player"));
+		if (hit.collider != null) {
+			rb.velocity = new Vector2 (speed * Mathf.Sign (forward.x), rb.velocity.y);
+		} else {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			isDead = true;
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private const double EPS = 1e-9;
+
+	private Rigidbody2D rb;
+	private bool isGrounded;
+	private bool isLeft = false;
+	private bool canJump = true;
+	private SpriteRenderer sr;
+
+	public float speed;
+	public float jumpSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		if (horizontal != 0)
+			Move (horizontal);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump ();
+		}
+
+	}
+
+	void Flip(){
+		sr.flipX = isLeft;
+	}
+
+	void Jump(){
+		if (!isGrounded && !canJump)
+			return;
+		else {
+			if (!isGrounded)
+				canJump = false;
+			rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
+			isGrounded = false;
+		}
+	}
+
+	void Move(float horizontal){
+		if (horizontal < -EPS)
+			isLeft = true;
+		else if (horizontal > EPS)
+			isLeft = false;
+		Flip ();
+
+		rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+	}
+
+
+	// Migué
+	void OnCollisionEnter2D (Collision2D col){
+		Check (col);
+	}
+
+	void OnCollisionStay2D (Collision2D col){
+		Check (col);
+	}
+
+	void OnCollisionExit2D (Collision2D col){
+		Check (col, false);
+	}
+
+	void Check(Collision2D col, bool check = true){
+		if (col.gameObject.tag == "Floor" || col.gameObject.tag == "Enemy") {
+			isGrounded = check;
+			canJump = true;
+		}
+	}
+}
