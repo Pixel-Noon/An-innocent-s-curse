ac2cee80c6d65b20c1e839fcda3b6cf50b572baf
Felipe <wowaccount123555@hotmail.com>
2016-11-14 22:39:26 -0200

    Umas nuvem marota aí, mas num ta muito legal não

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BackgroundController : MonoBehaviour {
+
+	public List<Sprite> cloudSprites;
+	public GameObject cloud; 
+	public float maxClouds;
+	public bool noClouds;
+
+	private bool isSpawning = false;
+	private List<GameObject> clouds;
+
+	// Use this for initialization
+	void Start () {
+		clouds = new List<GameObject> ();
+		if (maxClouds == 0)
+			maxClouds = 1000;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject c in clouds){
+			Vector3 p = Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, 10));
+			if (c.transform.position.x < p.x) {
+				clouds.Remove (c);
+			}
+
+			if (c.transform.position.x < -10) {
+				Destroy (c);
+				break;
+			}
+		}
+
+		if (clouds.Count < maxClouds && !isSpawning && !noClouds) {
+			isSpawning = true;
+			StartCoroutine (SpawnCloud ());
+		}
+	}
+
+	IEnumerator SpawnCloud(){
+		Sprite s = cloudSprites [Random.Range (0, cloudSprites.Count)];
+		float speed = Random.Range (1f, 2f);
+		float offset = Random.Range (0f, 100f);
+		float scale = Random.Range (0.8f, 1.2f);
+
+		float height = Camera.main.pixelHeight;
+		float width = Camera.main.pixelWidth;
+
+		Vector3 p = Camera.main.ScreenToWorldPoint (new Vector3 (width, height-offset, 10));
+		GameObject go = (GameObject) Instantiate (cloud, p, Quaternion.identity);
+		print (p);
+		Cloud c = go.GetComponent<Cloud> ();
+		c.setSprite (s);
+		c.setSpeed (speed/scale);
+		c.setScale (scale);
+		clouds.Add (go);
+
+		float seconds = Random.Range (1f, 3f);
+
+		yield return new WaitForSeconds (seconds);
+		isSpawning = false;
+	}
+}
---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cloud : MonoBehaviour {
+
+	private SpriteRenderer sr;
+	private Rigidbody2D rb;
+
+	public void setSprite(Sprite cloudSprite){
+		sr = GetComponent<SpriteRenderer> ();
+		sr.sprite = cloudSprite;
+	}
+
+	public void setSpeed(float speed){
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = new Vector2 (-speed, 0);
+	}
+
+	public void setScale(float scale){
+		transform.localScale = new Vector3 (scale, scale, scale);
+	}
+}
